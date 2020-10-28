0596b54155c4c590c76660404415ae2a9ab3e21d
becca <rtviner@gmail.com>
2020-10-28 12:33:32 -0600

    finished add session middleware, changed created_at to completed_on

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -3,7 +3,7 @@ const db = require('../models/fitnessTrackerModels');
 const sessionController = {};
 
 
-sessionController.getSessions = (req, res, next) => {
+sessionController.getAllSessions = (req, res, next) => {
   next();
 }
 
@@ -11,9 +11,24 @@ sessionController.getSessionByDate = (req, res, next) => {
   next()
 }
 
-sessionController.addSession = (req, res, next) => {
+sessionController.addSession = (req, res, next) => { 
+  const newSessionQuery = `
+    INSERT INTO sessions (completed_on, exercises, notes, user_id)
+    VALUES ($1,$2,$3,$4)
+    RETURNING session_id`;
 
-  next();
+  const { completed_on, exercises, notes, user_id } = req.body;
+  const sessionValues = [completed_on, exercises, notes, user_id];
+
+  db.query(newSessionQuery, sessionValues)
+    .then((data) => {
+      console.log("new session has been added: ", data.rows[0]);
+      res.locals.session = data.rows[0];
+      return next();
+    })
+    .catch((err) => {
+      return next({err})
+    });
 };
 
 module.exports = sessionController;
