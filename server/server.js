fb586947518d6ec0a078f9c83aa2f965d3c39c08
becca <rtviner@gmail.com>
2020-10-27 18:07:34 -0600

    outline main routes and setup error handlers

--- server/server.js
+++ server/server.js
@@ -3,25 +3,55 @@ const app = express();
 const PORT = 3000;
 
 const path = require('path');
+const sessionController = require('./controllers/sessionController');
+const userController = require('./controllers/userController');
 
+// parse request body
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
 // SETUP ROUTES
 
-// create a new user 
+// get root
 
-// create a new session
+app.get('/', (req, res) => {
+  // render main app or login page?
+});
+
+// if a new user signs in, create a new user 
+app.post('/signup', userController.createUser, (req, res) => {
+  // go to sessions page...
+
+});
 
 // look up all sessions by user_id (just have 1 user to start (user_id = 1))
-app.get('/sessions')
+app.get('/sessions', sessionController.getAllSessions, (req, res) => {
+
+});
 
 // look up a specific session with date and user_id (user_id = 1)
 app.get('/session')
 
+// create a new session
 
 
+// error handler for unknown route
+app.use((req, res) => res.sendStatus(404));
+
+//global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured'}
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
+
+// start server
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
