b2b9c47c2efd42fbe785a0c23054d5d35f392733
becca <rtviner@gmail.com>
2020-10-28 12:10:17 -0600

    completed create user logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,26 @@ const userController = {};
 
 // create and save a new user in the db
 userController.createUser = (req, res, next) => {
-  next();
+  const newUserQuery = `
+    INSERT INTO
+    users (username, password) 
+    VALUES ($1, $2)
+    RETURNING user_id`;
+
+  const { username, password } = req.body;
+
+  const userValues = [username, password];
+ 
+  db.query(newUserQuery, userValues)
+    .then((data) => {
+      console.log("new user has been inserted:", data.rows[0]);
+      res.locals.user = data.rows[0];
+      return next();
+    })
+    .catch((err) => {
+      console.log("global error handler will be called");
+      return next({err});
+    })
 }
 
 // make sure username and password from req.body match those found in the db at that username...
