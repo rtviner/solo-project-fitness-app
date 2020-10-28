4863a6d035edd8b4ef1d9ffa068046d16bd7bced
becca <rtviner@gmail.com>
2020-10-28 13:05:37 -0600

    completed get all sessions, returns all sessions for that user

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -4,7 +4,24 @@ const sessionController = {};
 
 
 sessionController.getAllSessions = (req, res, next) => {
-  next();
+  // select all sessions for a specific user and return array of json objects as response?
+  let id = req.params.id; 
+  // select all sessions where user_id = id
+  const allSessionsQuery = `
+    SELECT * 
+    FROM sessions 
+    WHERE user_id = $1`;
+  const values = [id];
+
+  db.query(allSessionsQuery, values)
+    .then((data) => {
+      console.log("data received: ", data.rows)
+      res.locals.sessions = data.rows;
+      return next();
+    })
+    .catch((err) => {
+      return next({err});
+    })
 }
 
 sessionController.getSessionByDate = (req, res, next) => {
