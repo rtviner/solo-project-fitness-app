1941ddaf48d9447872839568b40845b9a05672aa
becca <rtviner@gmail.com>
2020-10-29 13:38:40 -0600

    added bcrypt to encrypt user password with salt and hash before adding to db in create user and to compare entered password to encrypted password in verify user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const userController = {};
 
+
 // create and save a new user in the db
 userController.createUser = (req, res, next) => {
   const newUserQuery = `
@@ -12,30 +13,63 @@ userController.createUser = (req, res, next) => {
     RETURNING user_id`;
 
   const { username, password } = req.body;
-
-  const userValues = [username, password];
+  console.log("og password: ", password);
+  // bcrypt password before passing to DB
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next({err});
+    bcrypt.hash(password, salt, (err, hash) => {
+      console.log("hash :", hash);
+      if (err) return next({err});
+      
+      const userValues = [username, hash];
  
-  db.query(newUserQuery, userValues)
-    .then((data) => {
-      console.log("new user has been inserted:", data.rows[0]);
-      res.locals.user = data.rows[0];
-      return next();
-    })
-    .catch((err) => {
-      console.log("global error handler will be called");
-      return next({err});
-    })
+      db.query(newUserQuery, userValues)
+        .then((data) => {
+          console.log("new user has been inserted:", data.rows[0]);
+          res.locals.user = data.rows[0].user_id;
+          return next();
+        })
+        .catch((err) => {
+          console.log("global error handler will be called");
+          return next({err});
+        })
+      });
+  });
+  
 }
 
 // make sure username and password from req.body match those found in the db at that username...
 userController.verifyUser = (req, res, next) => {
   // find user by req.body.username
-  // get salt from bcrypt...
-  // check if users password matches req.body.password + salt bcrypted...
-  // if so res.locals.user = res data.id
-  // if not redirect to signup page
+  const { username, password } = req.body;
+  const findUserByNameQuery = `
+    SELECT * 
+    FROM users 
+    WHERE username = $1`;
+  const values = [username];
 
-  next();
+  db.query(findUserByNameQuery, values)
+    .then((data) => {
+      if (!data) res.redirect('/');
+      else {
+        const hash = data.rows[0].password;
+        console.log("hash from db: ", hash);
+        // check if users password matches req.body.password + salt bcrypted...
+        bcrypt.compare(password, hash)
+          .then((result) => {
+            console.log("password match result: ", result);
+            if (result === true) {
+              res.locals.user = data.rows[0].user_id;
+              return next();
+            }
+            // if not redirect to signup page
+            res.redirect('/')
+          })
+          .catch((err) => {
+            return next({err});
+          })
+      }
+    });
 }
 
 module.exports = userController;
