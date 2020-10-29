349b1a8c6277d472b2441cf059a3ed08c3402122
becca <rtviner@gmail.com>
2020-10-29 11:08:06 -0600

    use cookie parser

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const cookieController = require('./controllers/cookieController');
 // parse request body
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
+app.use(cookieParser());
 
 // SETUP ROUTES
 
