e9766f4c7a5fc9399c0f7b198f0061123b926927
becca <rtviner@gmail.com>
2020-10-29 13:37:05 -0600

    got start cookie session, delete cookie session, and verify session working

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -6,17 +6,19 @@ cookieController.setSSIDcookie = (req, res, next) => {
   // create a new browser cookie with ssid value set to res.locals.user
   const { user } = res.locals;
   res.cookie('ssid', user, { httpOnly: true });
+  console.log("Cookie created successfully: ", res.cookie.ssid);
   return next();
 };
 
 cookieController.startCookieSession = (req, res, next) => {
   // create a new instance of a cookie session with req.cookie.ssid as ssid...
   const newCookieSessionQuery = `
-  INSERT INTO cookie_sessions (cookie_id)
-  VALUES $1
-  RETURNING created_at`;
+    INSERT INTO 
+    cookie_sessions (cookie_id)
+    VALUES ($1)
+    RETURNING created_at`;
 
-  const { ssid } = req.cookie;
+  const ssid = res.locals.user;
   const values = [ssid];
 
   db.query(newCookieSessionQuery, values)
@@ -27,13 +29,32 @@ cookieController.startCookieSession = (req, res, next) => {
     .catch((err) => {
       return next({err});
     });
+};
 
+cookieController.deleteCookieSession = (req, res, next) => {
+  const deleteCookieById = `
+    DELETE FROM cookie_sessions
+    WHERE cookie_id = $1
+    RETURNING *`;
+  const user = req.cookies.ssid;
+  console.log("curr user: ", user);
+
+  const values = [user];
+
+  db.query(deleteCookieById, values)
+    .then((data) => {
+      console.log('Cookie session deleted: ', data.rows);
+      return next();
+    })
+    .catch((err) => {
+      return next({err});
+    })
 };
 
 cookieController.verifySession = (req, res, next) => {
   // find cookie session by cookie_id in the db
   const cookieById = `
-    SELECT * FROM cookieSessions
+    SELECT * FROM cookie_sessions
     WHERE cookie_id = $1`;
   //  use req.params.id for cookie_id value
   const { id } = req.params;
@@ -42,7 +63,7 @@ cookieController.verifySession = (req, res, next) => {
   db.query(cookieById, values)
     .then((data) => {
       // if no cookie session or cookie_id does not match req.cookies.ssid redirect to get signup route
-      if (!data || data.cookie_id !== req.cookies.ssid) {
+      if (!data.rows.length || data.rows[0].cookie_id !== req.cookies.ssid) {
         return res.status(400).redirect('/');
       }
       return next();
